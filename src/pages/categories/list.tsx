dd1c26a6fa352c26f0ed29c37798405ede91b134
senadev42 <senadevv42@gmail.com>
2023-07-21 03:16:08 +0300

    de-inferencing pages

--- src/pages/categories/list.tsx
+++ src/pages/categories/list.tsx
@@ -82,9 +82,9 @@ export const CategoryList: React.FC<IResourceComponentsProps> = () => {
           Create
         </button>
       </div>
-      <div className="max-w-full overflow-y-scroll">
-        <table className="w-full">
-          <thead>
+      <div className="max-w-full">
+        <table className="w-1/2">
+          {/* <thead>
             {getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
@@ -98,8 +98,8 @@ export const CategoryList: React.FC<IResourceComponentsProps> = () => {
                 ))}
               </tr>
             ))}
-          </thead>
-          <tbody className="divide-y divide-gray-200 text-center items-center">
+          </thead> */}
+          <tbody className="items-center">
             {getRowModel().rows.map((row) => (
               <tr key={row.id}>
                 {row.getVisibleCells().map((cell) => (
@@ -111,69 +111,6 @@ export const CategoryList: React.FC<IResourceComponentsProps> = () => {
             ))}
           </tbody>
         </table>
-      </div>
-      <div className="mt-4 flex items-center justify-between">
-        <div className="flex gap-4">
-          <button
-            className="px-4 py-2 bg-teal-500 text-white rounded hover:bg-teal-600"
-            onClick={() => setPageIndex(0)}
-            disabled={!getCanPreviousPage()}
-          >
-            {"<<"}
-          </button>
-          <button
-            className="px-4 py-2 bg-teal-500 text-white rounded hover:bg-teal-600"
-            onClick={() => previousPage()}
-            disabled={!getCanPreviousPage()}
-          >
-            {"<"}
-          </button>
-          <button
-            className="px-4 py-2 bg-teal-500 text-white rounded hover:bg-teal-600"
-            onClick={() => nextPage()}
-            disabled={!getCanNextPage()}
-          >
-            {">"}
-          </button>
-          <button
-            className="px-4 py-2 bg-teal-500 text-white rounded hover:bg-teal-600"
-            onClick={() => setPageIndex(getPageCount() - 1)}
-            disabled={!getCanNextPage()}
-          >
-            {">>"}
-          </button>
-          <span>
-            <strong>
-              {" "}
-              {getState().pagination.pageIndex + 1} / {getPageCount()}{" "}
-            </strong>
-          </span>
-        </div>
-        <div className="flex gap-4">
-          <span>Go to Page:</span>
-          <input
-            type="number"
-            defaultValue={getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              setPageIndex(page);
-            }}
-            className="w-16 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
-          />
-          <select
-            value={getState().pagination.pageSize}
-            onChange={(e) => {
-              setPageSize(Number(e.target.value));
-            }}
-            className="border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500"
-          >
-            {[10, 20, 30, 40, 50].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
-        </div>
       </div>
     </div>
   );
--- src/pages/categories/show.tsx
+++ src/pages/categories/show.tsx
@@ -1,6 +1,44 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { HeadlessShowInferencer } from "@refinedev/inferencer/headless";
+import React from "react";
+import {
+  useShow,
+  useResource,
+  useNavigation,
+  IResourceComponentsProps,
+} from "@refinedev/core";
 
 export const CategoryShow: React.FC<IResourceComponentsProps> = () => {
-  return <HeadlessShowInferencer />;
+  const { edit, list } = useNavigation();
+  const { id } = useResource();
+  const { queryResult } = useShow();
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  return (
+    <div style={{ padding: "16px" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1>Category Show</h1>
+        <div style={{ display: "flex", gap: "8px" }}>
+          <button onClick={() => list("categories")}>Categories</button>
+          <button onClick={() => edit("categories", id ?? "")}>Edit</button>
+        </div>
+      </div>
+      <div>
+        <div style={{ marginTop: "6px" }}>
+          <h5>Id</h5>
+          <div>{record?.id ?? ""}</div>
+        </div>
+        <div style={{ marginTop: "6px" }}>
+          <h5>Name</h5>
+          <div>{record?.name}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
